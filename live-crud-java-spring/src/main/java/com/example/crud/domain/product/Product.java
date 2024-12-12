66bc1370b83e94e114a8922569641125abeb3c6f
Brenda Bragança <100091048+BrendaBraganca@users.noreply.github.com>
2024-12-12 11:38:30 -0300

    Add files via upload

---
+++ live-crud-java-spring/src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,34 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name="product")
+@Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+    private Boolean active;
+
+    public Product(RequestProduct requestProduct){
+        this.name = requestProduct.name();
+        this.price_in_cents = requestProduct.price_in_cents();
+        this.active = true;
+    }
+}
